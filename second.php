ca604cd5b32e79c63330a56f1ec7d4576dbdedb7
kristelg <kristelg@greeny.cs.tlu.ee>
2016-09-15 11:27:47 +0300

    added login page

---
+++ second.php
@@ -0,0 +1,37 @@
+<?php
+
+	$age = 5;
+	
+	if($age < 18) {
+		
+		//true - on väiksem kui 18
+		echo "alaealine";
+	
+	} else {
+		// muul juhul
+		echo "täisealine";
+		
+	}	
+
+?>
+
+<br>
+<?php
+
+	for ($i = 0; $i < $age; $i = $i + 1) {
+		
+		echo ($i+1)."palju";
+	}
+	
+	echo "õnne!";
+
+?>
+
+<br>
+
+<?php
+
+	echo date("l d. m. Y g:i");
+
+	
+?>
