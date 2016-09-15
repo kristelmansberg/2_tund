f7af1e8262187ee3685a1ba4d60c3df8e073c0be
kristelg <kristelg@greeny.cs.tlu.ee>
2016-09-15 12:45:38 +0300

    added login page

--- login.php
+++ login.php
@@ -1,3 +1,25 @@
+<?Php
+	
+	//var_dump($_GET);
+	//echo "<br>";
+	//var_dump ($_POST);
+	
+	
+	$signupEmailError = "";
+	
+	//kas on üldse olemas
+	if (isset ($_POST ["signupEmail"])){
+		
+		//oli olemas, ehk keegi vajutas nuppu
+		//kas oli tühi
+		if (empty ($_POST ["signupEmail"])){
+			
+			//oli tõesti tühi
+			$signupEmailError = "See väli on kohustuslik";
+		}
+		
+	}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,18 +29,32 @@
 
 		<h1>Logi sisse</h1>
 		
-		<form>
-		
-			<input type="email">
+		<form method="POST">
+			<!--label>E-post</label>-->
+			<input placeholder= "E-post" name="loginEmail" type="email">
 			
 			<br> <br>
-			
-			<input type="password">
+			<!--label>Parool</label>-->
+			<input placeholder= "Parool" name="loginPassword" type="password">
 			
 			<br> <br>
 			
 			<input type="submit">
 		</form>
-
+		
+		<h1>Loo kasutaja</h1>
+		
+		<form method="POST">
+			<!--label>E-post</label>-->
+			<input placeholder= "E-post" name="signupEmail" type="email"><?php echo $signupEmailError; ?>
+			
+			<br> <br>
+			<!--label>Parool</label>-->
+			<input placeholder= "Parool" name="signupPassword" type="password">
+			
+			<br> <br>
+			
+			<input type="submit" value="Loo kasutaja">
+		</form>
 	</body>
 </html>
